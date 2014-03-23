3f5f214cb6243b8975a418be73a2eca5510cae84
Johnny Wu <KarasAya@gmail.com>
2014-03-23 16:36:25 +0800

    update dev shell

--- mvim_dev.sh
+++ mvim_dev.sh
@@ -1,5 +1,5 @@
 mvim \
     -u .vimrc \
     --cmd "let g:exvim_dev=1" \
-    --cmd "set runtimepath=./vimfiles,/Applications/MacVim.app/Contents/Resources/vim/vimfiles,/Applications/MacVim.app/Contents/Resources/vim/runtime,/Applications/MacVim.app/Contents/Resources/vim/vimfiles/after,./vimfiles/after" \
+    --cmd "set runtimepath=./vimfiles,\$VIMRUNTIME,./vimfiles/after" \
     $1
