2f1796a1ad03f7d2aac70b870c3762dd7188dc37
Johnny Wu <KarasAya@gmail.com>
2014-03-24 15:00:02 +0800

    update test environment

---
+++ copy_to_home.sh
@@ -0,0 +1,4 @@
+cp .vimrc ~/.vimrc
+cp .vimrc.bundles ~/.vimrc.bundles
+rm -rf ~/.vim
+cp -r vimfiles ~/.vim
--- gvim_dev.sh
+++ gvim_dev.sh
@@ -1,5 +1,6 @@
+cwd=`pwd`
 gvim \
-    -u .vimrc \
+    -u ${cwd}/.vimrc \
     --cmd "let g:exvim_dev=1" \
-    --cmd "set runtimepath=./vimfiles,\$VIMRUNTIME,./vimfiles/after" \
+    --cmd "set runtimepath=${cwd}/vimfiles,\$VIMRUNTIME,${cwd}/vimfiles/after" \
     $1
