9d9f62ea63bb845c36b733b4fe51b4b019c32732
jwu <karasaya@gmail.com>
2014-03-26 21:23:44 +0800

    update replace my vim

--- windows/replace-my-vim.bat
+++ windows/replace-my-vim.bat
@@ -1,8 +1,10 @@
 @echo off
 
-xcopy /Y .vimrc %HOMEPATH%\.vimrc
-xcopy /Y .vimrc.plugins %HOMEPATH%\.vimrc.plugins
-rmdir /S /Q %HOMEPATH%\.vim
-xcopy /Y /E vimfiles %HOMEPATH%\.vim\
+set HOME=%HOMEDRIVE%%HOMEPATH%
+
+xcopy /Y .vimrc %HOME%\.vimrc
+xcopy /Y .vimrc.plugins %HOME%\vimrc.plugins
+rmdir /S /Q %HOME%\.vim
+xcopy /Y /E vimfiles %HOME%\.vim\
 
 @echo on
