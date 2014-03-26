43be3357b342fd0e1d23c9f02115ce0b9914af19
jwu <karasaya@gmail.com>
2014-03-26 21:43:04 +0800

    update

--- windows/replace-my-vim.bat
+++ windows/replace-my-vim.bat
@@ -3,8 +3,8 @@
 REM  Reference: http://www.wilsonmar.com/1envvars.htm
 set home=%USERPROFILE%
 
-xcopy /Y .vimrc %home%\.vimrc
-xcopy /Y .vimrc.plugins %home%\vimrc.plugins
+copy /Y .vimrc %home%\.vimrc
+copy /Y .vimrc.plugins %home%\.vimrc.plugins
 rmdir /S /Q %home%\.vim
 xcopy /Y /E vimfiles %home%\.vim\
 
