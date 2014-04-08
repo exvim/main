85113f627c71b95efebc2832c58646f1b5455616
Johnny Wu <KarasAya@gmail.com>
2014-04-08 18:52:34 +0800

    small update

--- vimfiles/tools/shell/bash/update-filelist.sh
+++ vimfiles/tools/shell/bash/update-filelist.sh
@@ -14,9 +14,10 @@ fi
 # get filelist
 echo "  |- generate ${TMP}"
 if test "${FOLDERS}" != ""; then
-    IFS=$','
     # NOTE: there still have files under root
     find ${FORCE_POSIX_REGEX_1} . -maxdepth 1 -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")" > "${TMP}"
+    # we use comma for for-loop. Because FOLDERS may include white-space folders
+    IFS=$','
     for d in ${FOLDERS}; do 
         find ${FORCE_POSIX_REGEX_1} "$d" -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")" >> "${TMP}"
     done
