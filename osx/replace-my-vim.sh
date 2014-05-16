52729f5785beb23583a40994661237ac91510322
Johnny Wu <KarasAya@gmail.com>
2014-05-16 11:51:34 +0800

    will copy .local file

--- osx/replace-my-vim.sh
+++ osx/replace-my-vim.sh
@@ -3,5 +3,7 @@
 cp ./dist/ctags_lang ~/.ctags
 cp .vimrc ~/.vimrc
 cp .vimrc.plugins ~/.vimrc.plugins
+cp .vimrc.local ~/.vimrc.local
+cp .vimrc.plugins.local ~/.vimrc.plugins.local
 rm -rf ~/.vim
 cp -r vimfiles ~/.vim
--- windows/replace-my-vim.bat
+++ windows/replace-my-vim.bat
@@ -6,6 +6,8 @@ set home=%USERPROFILE%
 copy /Y .\dist\ctags_lang %home%\.ctags
 copy /Y .vimrc %home%\.vimrc
 copy /Y .vimrc.plugins %home%\.vimrc.plugins
+copy /Y .vimrc.local %home%\.vimrc.local
+copy /Y .vimrc.plugins.local %home%\.vimrc.plugins.local
 rmdir /S /Q %home%\.vim
 xcopy /Y /E vimfiles %home%\.vim\
 
