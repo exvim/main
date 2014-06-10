3b6ca5420a0a0b9c3ba6df8aa7ab553efdb67fb6
Wu Jie <KarasAya@gmail.com>
2014-06-11 00:56:20 +0800

    update distribution scripts

---
+++ dist/gen-package.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export version="v0.3.0" 
+export ORIGINAL_PATH=`pwd`
+
+if [ ! -d "~/exvim/build/" ]; then
+    mkdir ~/exvim/build/
+fi
+
+rsync -Cavz --exclude=".git*" --exclude=".DS_Store" --exclude="*.exvim" . ~/exvim/build/exvim-${version}
+
+cd ~/exvim/build/exvim-${version}
+tar -cvzf ~/exvim/build/exvim-${version}.tar.gz * .vimrc .vimrc.plugins .vimrc.mini
+zip -r ~/exvim/build/exvim-${version}.zip * .vimrc .vimrc.plugins .vimrc.mini
+
+cd ${ORIGINAL_PATH}
