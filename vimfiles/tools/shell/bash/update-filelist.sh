add3c699c0bca54a6bf0cc52cb7cf28d5ff0dc56
Johnny Wu <KarasAya@gmail.com>
2014-07-23 00:45:56 +0800

    fix find files will include folders

--- vimfiles/tools/shell/bash/update-filelist.sh
+++ vimfiles/tools/shell/bash/update-filelist.sh
@@ -17,16 +17,16 @@ fi
 echo "  |- generate ${TMP}"
 if test "${FOLDERS}" != ""; then
     # NOTE: there still have files under root
-    find ${FORCE_POSIX_REGEX_1} . -maxdepth 1 -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")" > "${TMP}"
+    find ${FORCE_POSIX_REGEX_1} . -type f -maxdepth 1 -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")" > "${TMP}"
     # we use comma for for-loop. Because FOLDERS may include white-space folders
     IFS=$','
     for d in ${FOLDERS}; do 
         unset IFS
-        find ${FORCE_POSIX_REGEX_1} "$d" -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")" >> "${TMP}"
+        find ${FORCE_POSIX_REGEX_1} "$d" -type f -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")" >> "${TMP}"
     done
     unset IFS
 else
-    find ${FORCE_POSIX_REGEX_1} . -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")" > "${TMP}"
+    find ${FORCE_POSIX_REGEX_1} . -type f -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")" > "${TMP}"
 fi
 
 # replace old file
