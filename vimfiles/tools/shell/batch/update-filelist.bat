2dac93782e47dea8b72526d65488232dc55cd12c
Lendy Zhang <LendyZhang@gmail.com>
2014-09-14 15:25:20 +0800

    Update batch files for Windows.

--- vimfiles/tools/shell/batch/update-filelist.bat
+++ vimfiles/tools/shell/batch/update-filelist.bat
@@ -2,19 +2,29 @@
 rem Create fileslist
 echo Creating Filelist...
 
+set SED_CMD="%TOOLS%\windows\sed.exe"
+if not exist %SED_CMD% (
+    set SED_CMD=sed
+)
+
+set GAWK_CMD="%TOOLS%\windows\gawk.exe"
+if not exist %GAWK_CMD% (
+    set GAWK_CMD=gawk
+)
+
 rem create cwd pattern for sed
 set CWD_PATTERN=%cd%
-for /f "delims=" %%a in ('echo %cd%^|sed "s,\\,\\\\,g"') do (
+for /f "delims=" %%a in ('echo %cd%^|%SED_CMD% "s,\\,\\\\,g"') do (
     set CWD_PATTERN=%%a
 )
 
 rem process
 echo   ^|- generate %TMP%
-dir /s /b %FILE_SUFFIXS%|sed "s,\(%CWD_PATTERN%\)\(.*\),.\2,gI" > "%TMP%"
+dir /s /b %FILE_SUFFIXS%|%SED_CMD% "s,\(%CWD_PATTERN%\)\(.*\),.\2,gI" > "%TMP%"
 
 echo   ^|- apply filter
 rem NOTE: dir /s /b *.cpp will list xxx.cpp~, too. So use gawk here to filter out thoes things.
-gawk -v filter_pattern=%FILE_FILTER_PATTERN% -v folder_filter=%FOLDER_FILTER_PATTERN% -f "%TOOLS%\gawk\file-filter-%GAWK_SUFFIX%.awk" "%TMP%">"%TMP2%"
+%GAWK_CMD% -v filter_pattern=%FILE_FILTER_PATTERN% -v folder_filter=%FOLDER_FILTER_PATTERN% -f "%TOOLS%\gawk\file-filter-%GAWK_SUFFIX%.awk" "%TMP%">"%TMP2%"
 del "%TMP%" > nul
 if exist "%TMP2%" (
     echo   ^|- move %TMP2% to %TARGET%
@@ -24,7 +34,7 @@ if exist "%TMP2%" (
 rem process id-utils files
 if exist "%TARGET%" (
     echo   ^|- generate %ID_TARGET%
-    gawk -f "%TOOLS%/gawk/null-terminal-files.awk" "%TARGET%">"%ID_TARGET%"
+    %GAWK_CMD% -f "%TOOLS%/gawk/null-terminal-files.awk" "%TARGET%">"%ID_TARGET%"
 )
 
 echo   ^|- done!
--- vimfiles/tools/shell/batch/update-idutils.bat
+++ vimfiles/tools/shell/batch/update-idutils.bat
@@ -2,6 +2,11 @@
 rem create ID
 echo Creating ID...
 
+set MKID_CMD="%TOOLS%\windows\mkid.exe"
+if not exist %MKID_CMD% (
+    set MKID_CMD=mkid
+)
+
 rem try to use auto-gen id language map
 echo   ^|- generate %TMP%
 if exist "%DEST%\id-lang-autogen.map" (
@@ -10,7 +15,7 @@ if exist "%DEST%\id-lang-autogen.map" (
 rem if auto-gen map not exists we use default one in tools directory
     set LANG_MAP=%TOOLS%\idutils\id-lang.map
 )
-mkid --file="%TMP%" --include="text" --lang-map="%LANG_MAP%" --prune="%EXCLUDE_FOLDERS%"
+%MKID_CMD% --file="%TMP%" --include="text" --lang-map="%LANG_MAP%" --prune="%EXCLUDE_FOLDERS%"
 
 rem replace old file
 if exist "%TMP%" (
